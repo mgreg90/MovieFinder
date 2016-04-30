3f156d1f7deb5c5e63cbdd58fbada1a312e1a291
Mike Gregory <mgregory8219@gmail.com>
2016-04-30 14:12:25 -0400

    finished guide

--- server.rb
+++ server.rb
@@ -2,10 +2,13 @@ require 'sinatra'
 require './models/siskel.rb'
 
 get '/' do
+  title = params[:title]
+  @movie = Siskel.new(title)
+  params.to_s
   erb :index
 end
 
 get '/movie' do
-  movie = Siskel.new("Lion King")
-  "This movie title is #{movie.title}"
+  @movie = Siskel.new("Lion King")
+  erb :index
 end
